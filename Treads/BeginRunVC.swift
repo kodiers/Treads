870bd15472f037e5a8592560e0e9cf16f5e1f1af
Victor Yamchinov <kodiers@gmail.com>
2020-01-15 19:54:04 +0300

    initial setup

--- Treads/BeginRunVC.swift
+++ Treads/BeginRunVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  BeginRunVC.swift
 //  Treads
 //
 //  Created by Viktor Yamchinov on 15.01.2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeginRunVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
