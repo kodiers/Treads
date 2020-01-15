dc14c2f52b9e5739d57efef0989e56410fad9bb5
Victor Yamchinov <kodiers@gmail.com>
2020-01-15 19:32:11 +0300

    Initial Commit

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Viktor Yamchinov on 15.01.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Viktor Yamchinov on 15.01.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
